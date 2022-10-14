55527f7a78aaca49c86a8f06b1bcb605b378f99a
MaVe0 <69248874+MaVe0@users.noreply.github.com>
2022-10-14 16:54:41 +0200

    manual for weighting added

--- R/function_sampling_nra_weight_calculation.R
+++ R/function_sampling_nra_weight_calculation.R
@@ -7,18 +7,33 @@
 # nonRespValue = value(s) of statusVar that represent non-responding cases for which responder weights are adjusted
 # respValue = value of statusVar that represents respondents
 
+#' Calculates non-response adjusted weights
+#'
+#' @param df Weighted data frame with nra cells defined; use nra_cells() for cell definition
+#' @param cellVar Name of nra cell variable
+#' @param weightVar Name of weight variable that needs adjustments
+#' @param nraWeightVar Name of output variable
+#' @param statusVar Name of integer variable containing participation codes, where one code represents respondents
+#' @param nonRespValue Integer or integer vector containing statusVar values that represent non-responding cases for which responder weights are adjusted
+#' @param respValue Integer for respVar value that represents responding cases
+#'
+#' @return Inout data frame with additional column nraWeightVar containing adjusted weights
+#' @import dplyr
+#' @export
+#'
+#' @examples
 nra_weights <- function(df, cellVar, weightVar, nraWeightVar, statusVar, nonRespValue, respValue) {
-  
+
   # get data frame and rename variables
   df <- df %>%
     rename(nraCell = all_of(cellVar), wgt = all_of(weightVar), status = all_of(statusVar))
-  
+
   # vector with all nra cells
   cells <- unique(df$nraCell)
-  
+
   # empty df that will grow
   nraFactors <- data.frame(NULL)
-  
+
   # loop calculating factors
   for(i in 1:length(cells)) {
     # filter current cell and group by status
@@ -40,12 +55,12 @@ nra_weights <- function(df, cellVar, weightVar, nraWeightVar, statusVar, nonResp
     cell <- select(cell, nraFactor) %>%
       mutate(nraCell = cells[i]) %>%
       unique()
-    
+
     # rbind factors to growing df
     nraFactors <- rbind(nraFactors, cell)
-    
+
   }
-  
+
   # bind factors to df
   df2 <- left_join(df, nraFactors, by = "nraCell") %>%
     mutate(nraWgt = case_when(
@@ -57,8 +72,8 @@ nra_weights <- function(df, cellVar, weightVar, nraWeightVar, statusVar, nonResp
       TRUE ~ wgt)) %>%
     # use desired out variable name
     rename(!!nraWeightVar := nraWgt, !!weightVar := wgt)
-  
+
   return(df2)
-  
+
 }
 
