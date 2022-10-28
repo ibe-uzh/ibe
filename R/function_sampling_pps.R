d1e6fb4f04cf54bf132ee1da9e449296db71a52a
MaVe0 <69248874+MaVe0@users.noreply.github.com>
2022-10-28 13:58:05 +0200

    flexible ids in certainity stratum

--- R/function_sampling_pps.R
+++ R/function_sampling_pps.R
@@ -123,8 +123,10 @@ sample_PPS <- function(df, enrVar, tcs, n, undersampling = FALSE, idStart = 1) {
                   sc_smp_scweight = round(1/sc_smp_scselprob, 4)
                   )
 
-  # ids for certainties  
-  certainties$id_school <- str_pad((length(sampVec) + idStart):(length(sampVec) + nrow(certainties) + idStart), 3, "left", pad = "0")
+  # ids for certainties
+  start <- as.numeric(max(str_sub(frame$id_school, 2, 3), na.rm = TRUE)) + 1
+  end <- start + nrow(certainties) - 1
+  certainties$id_school <- str_pad(start:end, 3, "left", pad = "0")
   
   # add samp vars to certainties
   certainties <- mutate(certainties,
