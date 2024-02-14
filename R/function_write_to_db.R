9160759d7622a640cb21db4f18f8c54d13d5fa62
MaVe0 <martin.verner@gmx.ch>
2024-02-14 08:42:39 +0100

    fixed problem with reserved sql words in write_df_to_db

--- R/function_write_to_db.R
+++ R/function_write_to_db.R
@@ -7,19 +7,23 @@
 #' @import DBI
 #' @export
 write_df_to_db <- function(df, conn_name, table_name) {
-  # save sessions as temporary file
+  # Save sessions as a temporary file
   tempPath <- tempfile("temporary_data_frame", fileext = ".csv")
   tempPath <- gsub("\\", "/", tempPath, fixed = TRUE)
   write.table(df, tempPath, row.names = FALSE, quote = FALSE, sep = "\t", col.names = FALSE)
   
-  # delete possible previous entries
+  # Delete possible previous entries
   query <- sprintf("DELETE FROM %s", table_name)
   dbExecute(conn_name, query) 
   
-  # upload temporary file to database (faster than dbWriteTable)
-  vars <- paste(colnames(df), collapse = ", ")
+  # Prepare column names, adding backticks around reserved words
+  vars <- colnames(df)
+  vars <- ifelse(vars %in% c("check", "index"), sprintf("`%s`", vars), vars)
+  vars_string <- paste(vars, collapse = ", ")
+  
+  # Upload temporary file to database (faster than dbWriteTable)
   query <- sprintf("LOAD DATA LOCAL INFILE '%s' INTO TABLE %s FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' LINES TERMINATED BY '\n' (%s)",
-                   tempPath, table_name, vars)
+                   tempPath, table_name, vars_string)
   dbExecute(conn_name, query)
   
   print(query)
