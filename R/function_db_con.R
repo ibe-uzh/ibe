98a842eeb83d87d68573c9cf2704afb992727b67
benimwolfspelz <benjamin.wolf@ibe.uzh.ch>
2024-05-07 11:59:22 +0200

    added DB "dateneingabe" to db_con function

--- R/function_db_con.R
+++ R/function_db_con.R
@@ -1,6 +1,6 @@
 #' Connect to IBEs databases
 #' 
-#' Wrapper around RMariaDB::dbConnect to connect to itembank, checkauswertung (sessions / results), and check_data_revisions (mutterdatei). An active SSH tunnel is required!
+#' Wrapper around RMariaDB::dbConnect to connect to itembank, checkauswertung (sessions / results), dateneingabe, and check_data_revisions (mutterdatei). An active SSH tunnel is required!
 #' 
 #' @param user Username to log on to DB server with.
 #' @param db Character: Which database to connect to. If several or "all" are chosen, the output will be a list of connections.
@@ -8,17 +8,18 @@
 #' @return A connection object of list of connection objects.
 #' 
 #' @export
-db_con <- function(user, db = c("all", "itembank", "checkauswertung", "mutterdatei", "tracking", "check_data_revisions") ) {
+db_con <- function(user, db = c("all", "itembank", "datenenigabe", "checkauswertung", "mutterdatei", "tracking", "check_data_revisions") ) {
   
   db <- match.arg(db, several.ok =  TRUE)
   db[db %in% c("mutterdatei", "tracking")] <- "check_data_revisions"
   db <- unique(db)
-  if (any(db == "all")) db <- c("itembank", "checkauswertung", "check_data_revisions")
+  if (any(db == "all")) db <- c("itembank", "dateneingabe", "checkauswertung", "check_data_revisions")
   pw <- rstudioapi::askForPassword(paste("DB server password for", user))
   cons <- list()
   for (dbi in db) {
     hostname <- switch(dbi,
                        itembank = "127.0.0.1",
+                       dateneingabe = "127.0.0.1",
                        checkauswertung = "127.0.0.1",
                        check_data_revisions = "ibedb-exoscale-a9317092-eb47-4f96-875c-c8641b2337e7.a.aivencloud.com")
     portnr = switch(dbi,
