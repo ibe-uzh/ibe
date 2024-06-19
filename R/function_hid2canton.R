469444e8481b8193a84cdc9d3996e189a5ad50a2
benimwolfspelz <benjamin.wolf@ibe.uzh.ch>
2024-06-19 14:27:55 +0200

    hid2canton now recognises FL correctly

--- R/function_hid2canton.R
+++ R/function_hid2canton.R
@@ -9,7 +9,7 @@
 hid2canton <- function(hid, full.name=FALSE) {
   dd <- data.frame(hid = c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13",
                            "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26",
-                           "32"),
+                           "27"),
                    canton1 = c("Aargau", "Appenzell Innerrhoden", "Appenzell Ausserrhoden", "Basel-Stadt", "Basel-Landschaft", "Bern",
                                "Fribourg", "Geneva", "Glarus", "Graubünden", "Jura", "Lucerne", "Neuchâtel", "Nidwalden", "Obwalden",
                                "Schaffhausen", "Schwyz", "Solothurn", "St. Gallen", "Thurgau", "Ticino", "Uri", "Valais", "Vaud", "Zug",
